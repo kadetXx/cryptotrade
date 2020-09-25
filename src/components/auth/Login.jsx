17049683145517500a6f4dcf8b9574c908d416c8
Kadet <techkadet@gmail.com>
2020-09-25 11:31:16 +0100

    fixed build errors

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import "./Auth.scss";
 import Alert from '../alert/Alert'
